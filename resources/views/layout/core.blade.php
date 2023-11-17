31045b19126d0ca73d564a1006dd38c12e840ff9
malvinval <malvinval09@gmail.com>
2023-11-17 19:11:50 +0700

    init commit

---
+++ resources/views/layout/core.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env("APP_NAME") }}</title>
+    <link rel="stylesheet" href="/css/style.css">
+
+    {{-- Tailwind CDN --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- Tailwind Config --}}
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              colors: {
+                clifford: '#da373d',
+              }
+            }
+          }
+        }
+    </script>
+</head>
+<body>
+    @include('partials/navbar')
+
+    @yield("container")
+</body>
+{{-- Navbar Javascript --}}
+<script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends("layout/core")
+
+@section("container")
+    <div class="container">
+        <p>Test Welcome</p>
+    </div>
+@endsection
