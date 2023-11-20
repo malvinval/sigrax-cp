d9d68e551d40cd92851e74a14f183ed9ecdd296a
malvinval <malvinval09@gmail.com>
2023-11-20 13:35:26 +0700

    update: navbar links active color, blogs page title

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -31,23 +31,23 @@
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-sigrax-yellow border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-sigrax-yellow lg:p-0 dark:text-sigrax-yellow lg:dark:hover:text-sigrax-yellow dark:hover:bg-gray-700 dark:hover:text-sigrax-yellow lg:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+                        class="block py-2 pl-3 pr-4 {{ Request::is('/') ? 'text-sigrax-yellow' : 'text-gray-300' }} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-sigrax-yellow lg:p-0 dark:text-sigrax-yellow lg:dark:hover:text-sigrax-yellow dark:hover:bg-gray-700 dark:hover:text-sigrax-yellow lg:dark:hover:bg-transparent dark:border-gray-700">Home</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-300 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        class="block py-2 pl-3 pr-4 {{ Request::is('about') ? 'text-sigrax-yellow' : 'text-gray-300' }} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-300 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Portfolio</a>
+                        class="block py-2 pl-3 pr-4 {{ Request::is('portfolio') ? 'text-sigrax-yellow' : 'text-gray-300' }} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Portfolio</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-300 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        class="block py-2 pl-3 pr-4 {{ Request::is('contact') ? 'text-sigrax-yellow' : 'text-gray-300' }} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                 </li>
                 <li>
                     <a href="/blogs"
-                        class="block py-2 pl-3 pr-4 text-gray-300 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Articles</a>
+                        class="block py-2 pl-3 pr-4 {{ Request::is('blog*') ? 'text-sigrax-yellow' : 'text-gray-300' }} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-400 lg:p-0 dark:text-gray-300 lg:dark:hover:text-red-400 dark:hover:bg-gray-700 dark:hover:text-red-400 lg:dark:hover:bg-transparent dark:border-gray-700">Our Blogs</a>
                 </li>
             </ul>
         </div>
