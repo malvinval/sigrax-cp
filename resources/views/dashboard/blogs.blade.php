8870da0669a100876c447d6c537dd94f3fa14279
malvinval <malvinval09@gmail.com>
2023-11-20 17:00:39 +0700

    add: view, edit, delete button in dashboard blogs

--- resources/views/dashboard/blogs.blade.php
+++ resources/views/dashboard/blogs.blade.php
@@ -70,17 +70,18 @@
               </div>
               <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="">{{ $b->title }}</a></h2>
               <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ $b->excerpt }}</p>
-              <div class="flex justify-between items-center">
+              <div class="flex flex-col md:flex-row justify-between md:items-center">
                   <div class="flex items-center space-x-4">
                       <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
                       <span class="font-medium dark:text-white">
                           by: {{ $b->author }}
                       </span>
                   </div>
-                  <a href="" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
-                      <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                  </a>
+                  <div class="pt-5">
+                    <a class="px-3 py-1 bg-blue-500 hover:bg-blue-600 text-white rounded-lg" href="">View</a>
+                    <a class="px-3 py-1 bg-yellow-500 hover:bg-yellow-600 text-white rounded-lg" href="">Edit</a>
+                    <a class="px-3 py-1 bg-red-500 hover:bg-red-700 text-white rounded-lg" href="">Delete</a>
+                  </div>
               </div>
           </article>                
       @endforeach
