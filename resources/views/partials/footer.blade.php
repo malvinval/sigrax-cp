5bd4105fa0c9bfed02306a069223e02d79fcc46f
malvinval <malvinval09@gmail.com>
2023-11-20 13:37:31 +0700

    update: footer padding

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="bg-white dark:bg-gray-900 font-jost">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+<footer class="bg-white dark:bg-gray-900 px-0 lg:px-10 font-jost w-full">
+    <div class="mx-auto w-full p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
