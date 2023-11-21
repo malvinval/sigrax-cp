c8f3cff03f35f80bf8783a27ed7e5552ecabd7ad
Hagi <Hagiazzamaz@gmail.com>
2023-11-21 19:29:45 +0700

    fix home margins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends("layout/core")
 
 @section("container")
-    <div class="hero min-h-screen">
+    <div class="hero">
         <div class="flex flex-col lg:flex-row text-gray-800 dark:text-white">
             <div class="flex flex-col flex-grow lg:h-screen text-cente font-jost grid place-items-center mx-auto sm:w-full lg:w-1/3 pt-24 lg:pt-0 pb-16 lg:pb-0 bg-gradient-to-b lg:bg-gradient-to-r from-red-500 to-slate-800">
                 <img class="pt-8 w-1/2 sm:w-1/4 lg:w-3/4 mx-auto drop-shadow-xl" src="{{ asset('images/sigrax-logo.png') }}" alt="Dashboard">            
@@ -14,12 +14,14 @@
                 </div>
             </div>
         </div>
-        <div class="w-full bg-white dark:bg-slate-800 font-jost text-center font-jost mx-auto pt-24 pb-16 md:pb-0 text-gray-800 dark:text-white">
+    </div>
+    <div class="w-full py-24 bg-white dark:bg-slate-800 font-jost">
+        <div class="text-center font-jost mx-auto w-3/4 pb-16 md:pb-0 text-gray-800 dark:text-white">
             <h1 class="font-bold text-6xl pb-8">Our Services</h1>
             <img class="py-4 w-4/5 lg:w-1/2 mx-auto drop-shadow-xl" src="{{ asset('images/sigrax-home.png') }}" alt="Dashboard">
-            <h3 class="font-medium w-3/4 mx-auto text-lg pt-8">SIGRAX EAMS (Enterprise Asset Management System) is a web-based system designed to assist in the planning, management, and administrative functions required for effective maintenance.</h3>
-            <h3 class="font-medium w-3/4 mx-auto text-lg pt-8">We offer a comprehensive SIGRAX CMMS software solution along with implementation services and training to ensure that the system purchased by our clients can be fully utilized to facilitate the asset management process within the company.</h3>
-            <h3 class="font-medium w-3/4 mx-auto text-lg pt-8">Our system is developed in Indonesia by a group of maintenance and information technology practitioners. They also bring extensive implementation experience.</h3>
+            <h3 class="font-medium text-lg pt-8">SIGRAX EAMS (Enterprise Asset Management System) is a web-based system designed to assist in the planning, management, and administrative functions required for effective maintenance.</h3>
+            <h3 class="font-medium text-lg pt-8">We offer a comprehensive SIGRAX CMMS software solution along with implementation services and training to ensure that the system purchased by our clients can be fully utilized to facilitate the asset management process within the company.</h3>
+            <h3 class="font-medium text-lg pt-8">Our system is developed in Indonesia by a group of maintenance and information technology practitioners. They also bring extensive implementation experience.</h3>
         </div>
     </div>
     <div class="min-h-[910px] w-full pt-24 bg-white dark:bg-slate-800 font-jost">
