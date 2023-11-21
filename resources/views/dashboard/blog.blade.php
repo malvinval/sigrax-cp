baeab29fd07583eaad28682c4dcdbd29f7efc6de
malvinval <malvinval09@gmail.com>
2023-11-21 20:46:58 +0700

    update: dark mode adjusment for view blog in dashboard

--- resources/views/dashboard/blog.blade.php
+++ resources/views/dashboard/blog.blade.php
@@ -10,7 +10,7 @@
         <a class="px-3 py-1 bg-red-500 hover:bg-red-700 text-white rounded-lg" href="">Delete</a>
       </div>
 
-    <main class="font-jost bg-white dark:bg-gray-900 antialiased">
+    <main class="font-jost bg-white  antialiased">
         <div class="flex">
             <article class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
                 <header class="mb-4 lg:mb-6 not-format">
@@ -18,13 +18,13 @@
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                             <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
                             <div>
-                                <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $blog->author }}</a>
+                                <a href="#" rel="author" class="text-xl font-bold text-gray-900">{{ $blog->author }}</a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">Sigrax CMMS</p>
                                 <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">created at {{ $blog->created_at }}</time></p>
                             </div>
                         </div>
                     </address>
-                    <h1 class="mb-4 text-3xl font-bold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{ $blog->title }}</h1>
+                    <h1 class="mb-4 text-3xl font-bold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{{ $blog->title }}</h1>
 
                     <div class="container text-justify">
                         {{ $blog->content }}
