943584aadc2686572ab78179d0f56990956c7c87
malvinval <malvinval09@gmail.com>
2023-11-24 18:02:38 +0700

    fix: logout confirmation alert message

--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -47,7 +47,7 @@
         
         <a href="/dashboard/manage-contacts" class="text-[15px] ml-4 p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white font-bold">Manage contacts</a>
 
-        <form onsubmit="return confirm('Do you really want to delete this blog?');" class="flex py-8 px-2" action="/logout" method="POST">
+        <form onsubmit="return confirm('Do you really want to logout?');" class="flex py-8 px-2" action="/logout" method="POST">
             @csrf
             <button class="bg-red-500 hover:bg-red-600 text-white py-1 rounded-lg cursor-pointer px-4 font-bold">Logout</button>
         </form>
