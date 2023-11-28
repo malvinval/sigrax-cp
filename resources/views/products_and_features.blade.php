c9e36945b3bb513733ec536cd0f008190202a70f
malvinval <malvinval09@gmail.com>
2023-11-28 12:52:37 +0700

    update: styling for products page

--- resources/views/products_and_features.blade.php
+++ resources/views/products_and_features.blade.php
@@ -15,11 +15,11 @@
                             <p class="text-red-500 font-bold py-24">No products yet.</p>
                         @endif
                     </div> 
-                    <div class="grid gap-8 lg:grid-cols-2">
+                    <div class="">
                         @foreach($products as $p)
                             <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                                 <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/features/{{ $p->slug }}">{{ $p->title }}</a></h2>
-                               
+                                <p class="my-5 text-justify">{{ $p->desc }}</p>
                                 <a href="/features/{{ $p->slug }}" class="text-white bg-blue-500 px-4 py-2 rounded-lg inline-flex items-center font-medium dark:text-white hover:underline">
                                     See features
                                     <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
